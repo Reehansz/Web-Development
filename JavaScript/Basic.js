2f1c15e0a558401c8e0c5183025e2b10dfe08039
Reehan Shaveez <reehanshaveez123@gmail.com>
2024-12-15 00:21:35 +0530

    BMI calculator

--- JavaScript/Basic.js
+++ JavaScript/Basic.js
@@ -1,3 +1,10 @@
-var myName = "ABC"
-console.log(myName)
-console.log("Number of characters is : " + myName.length)
+var myName = "ABC";
+console.log(myName);
+console.log("Number of characters is : " + myName.length);
+
+function bmiCalculator(weight, height){
+    var bmi = weight / (height**2);
+    return Math.round(bmi);
+}
+
+console.log(bmiCalculator(80,1.76));
